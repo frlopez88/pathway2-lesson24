80dd0424ac5d11f4ba095edfbae0eadffcdb12e1
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-10-22 19:40:49 -0600

    Summary Commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let incomeModal
 let expenseModal
 let endPoint = `https://qnfzizaesdfdijygrkkh.supabase.co/rest/v1/finances`
 let token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZnppemFlc2RmZGlqeWdya2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzNDczNzIsImV4cCI6MjA0MzkyMzM3Mn0.llAqlwkL2QvoEaHCq9b9yf0Thp3F31xlA2f-QGx0lGk`
+let incomeSummary = 0
+let expenseSumary = 0
+
 
 
 const logIn = ()=>{
@@ -63,6 +66,88 @@ const createTransaction = async () => {
 
 }
 
+
+async function getIncomes(){
+
+    let username = window.localStorage.getItem("username")
+
+    let url = `${endPoint}?user=eq.${username}&type=eq.Income`
+
+    let response = await fetch(url, {
+        method : 'GET',
+        headers: {
+            'apikey': token, 
+            'Authorization' : token
+        }
+    })
+
+    let body = await response.json()
+
+    
+
+    if(response.ok){
+
+        for(let i =0;  i < body.length; i++){
+            incomeSummary += body[i].amount
+        }
+        outPutTotalIncome.innerHTML  = `$ ${incomeSummary}`
+    }
+
+    
+
+}
+
+
+
+async function getExpenses(){
+
+    let username = window.localStorage.getItem("username")
+
+    let url = `${endPoint}?user=eq.${username}&type=eq.Expense`
+
+    let response = await fetch(url, {
+        method : 'GET',
+        headers: {
+            'apikey': token, 
+            'Authorization' : token
+        }
+    })
+
+    let body = await response.json()
+
+    
+    if(response.ok){
+
+        for(let i =0;  i < body.length; i++){
+            expenseSumary += body[i].amount
+        }
+        outPutTotalExpense.innerHTML  = `$ ${expenseSumary}`
+    }
+
+
+
+}
+
+
+
+
+async function getTotalBalance(){
+
+    await getIncomes()
+    await getExpenses()
+
+    let totalBalance = incomeSummary - expenseSumary
+   
+
+    outPutTotal.innerHTML = `$ ${totalBalance}`
+
+}
+
+
+
+
+getTotalBalance()
+
 function hideShowCategory() {
 
     let type = inputType.value
@@ -97,6 +182,8 @@ function showIncomeModal() {
     incomeModal.show()
 
 }
+
+
 
 
 function showExpensesModal() {
